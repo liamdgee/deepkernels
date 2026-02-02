6c7757fe4e87e40d5bc970fb2c7ab5e7c4df5226
liamdgee <liamdgiles@outlook.com>
2026-02-02 14:50:39 +1100

    orchestration class patch

--- src/preprocess/pipe.py
+++ src/preprocess/pipe.py
@@ -1,27 +1,27 @@
 from sklearn.pipeline import Pipeline
 from src.preprocess import cleaner, lasso, preprocess, harmoniser, novelty, inducing
+from pydantic import Field, BaseModel
 
-class DataOrchestrator:
-    def __init__(self, config):
-        self.config = config
-    def _run_processes(self, config):
-        clean = cleaner.DataCleaner(config=cleaner.CleanerConfig())
-        harmonise = harmoniser.SchemaHarmoniser(config=harmoniser.HarmoniserConfig()) #-- i dont think harmonise currently exists in yaml--#
-        custom = novelty.FeatureTransformer(config=lasso.FeatureEngConfig())
-        select = lasso.LassoFeatures(config=lasso.FeatureEngConfig())
-        process = preprocess.TorchPreprocessor(config=preprocess.PreprocessConfig())
-        induce = inducing.InducingPointSelect(config=inducing.InducingConfig)
-
-        pipe = Pipeline([
-            ('individual_df_cleaning', clean),
-            ('harmonise_schemas', harmonise),
-            ('feature_engineering', custom),
-            ('stat_feature_select', select),
-            ('inducing_point_select', induce),
-            ('torch_tensor_format', process)
-        ])
-
-        return pipe
+class DataOrchestrator(BaseModel):
+    def __init__(self, cleanconfig=None, harmoniseconfig=None, featureconfig=None, induceconfig=None, processconfig=None):
+        self.cleanconfig = cleanconfig or cleaner.CleanerConfig()
+        self.harmoniseconfig = harmoniseconfig or harmoniser.HarmoniserConfig()
+        self.featureconfig = featureconfig or lasso.FeatureEngConfig()
+        self.induceconfig = induceconfig or inducing.InducingConfig()
+        self.processconfig = processconfig or preprocess.PreprocessConfig()
+    
+    def _run_processes(
+            self, **overrides
+        ):
+        arch = [
+            ('cleaning', overrides.get('clean', cleaner.DataCleaner(self.cleanconfig))),
+            ('harmonising', overrides.get('harmonise', harmoniser.SchemaHarmoniser(self.harmoniseconfig))),
+            ('feat_eng', overrides.get('custom', novelty.FeatureTransformer(self.featureconfig))),
+            ('feat_select', overrides.get('select', lasso.LassoFeatures(self.featureconfig))),
+            ('inducing_points', overrides.get('induce', inducing.InducingPointSelect(self.induceconfig))),
+            ('torch_process', overrides.get('process', preprocess.TorchPreprocessor(self.processconfig)))
+        ]
+        return Pipeline(arch)
 
 
 
