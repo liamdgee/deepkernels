a8be27a71456d6614d948c059c9dc176f22c033b
liamdgee <liamdgiles@outlook.com>
2026-02-02 15:16:07 +1100

    further orchestration changes

--- src/preprocess/pipe.py
+++ src/preprocess/pipe.py
@@ -1,25 +1,28 @@
 from sklearn.pipeline import Pipeline
 from src.preprocess import cleaner, lasso, preprocess, harmoniser, novelty, inducing
 from pydantic import Field, BaseModel
+from src.preprocess import cleaner, lasso, preprocess, harmoniser, novelty, inducing
 
-class DataOrchestrator(BaseModel):
-    def __init__(self, cleanconfig=None, harmoniseconfig=None, featureconfig=None, induceconfig=None, processconfig=None):
-        self.cleanconfig = cleanconfig or cleaner.CleanerConfig()
-        self.harmoniseconfig = harmoniseconfig or harmoniser.HarmoniserConfig()
-        self.featureconfig = featureconfig or lasso.FeatureEngConfig()
-        self.induceconfig = induceconfig or inducing.InducingConfig()
-        self.processconfig = processconfig or preprocess.PreprocessConfig()
+class ProcessConfig(BaseModel):
+    clean: cleaner.CleanerConfig = Field(default_factory=cleaner.CleanerConfig)
+    harmonise: harmoniser.HarmoniserConfig = Field(default_factory=harmoniser.HarmoniserConfig)
+    feature: lasso.FeatureEngConfig = Field(default_factory=lasso.FeatureEngConfig)
+    induce: inducing.InducingConfig = Field(default_factory=inducing.InducingConfig)
+    process: preprocess.PreprocessConfig = Field(default_factory=preprocess.PreprocessConfig)
+
+class DataOrchestrator:
+    def __init__(self, config=None):
+        self.config = config if config is not None else ProcessConfig()
     
-    def _run_processes(
-            self, **overrides
-        ):
+    def build_pipe(self, **overrides):
+
         arch = [
-            ('cleaning', overrides.get('clean', cleaner.DataCleaner(self.cleanconfig))),
-            ('harmonising', overrides.get('harmonise', harmoniser.SchemaHarmoniser(self.harmoniseconfig))),
-            ('feat_eng', overrides.get('custom', novelty.FeatureTransformer(self.featureconfig))),
-            ('feat_select', overrides.get('select', lasso.LassoFeatures(self.featureconfig))),
-            ('inducing_points', overrides.get('induce', inducing.InducingPointSelect(self.induceconfig))),
-            ('torch_process', overrides.get('process', preprocess.TorchPreprocessor(self.processconfig)))
+            ('cleaning', overrides.get('clean', cleaner.DataCleaner(self.config.clean))),
+            ('harmonising', overrides.get('harmonise', harmoniser.SchemaHarmoniser(self.config.harmonise))),
+            ('feat_eng', overrides.get('custom', novelty.FeatureTransformer(self.config.feature))),
+            ('feat_select', overrides.get('select', lasso.LassoFeatures(self.config.feature))),
+            ('inducing_points', overrides.get('induce', inducing.InducingPointSelect(self.config.induce))),
+            ('torch_process', overrides.get('process', preprocess.TorchPreprocessor(self.config.process)))
         ]
         return Pipeline(arch)
 
