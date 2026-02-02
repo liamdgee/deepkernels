9f35361faa142879a2d6a35d7db67ad66bf928fd
liamdgee <liamdgiles@outlook.com>
2026-02-02 15:38:14 +1100

    fixed core git logic

--- main.py
+++
@@ -1,9 +0,0 @@
-#---Dependencies---#
-import yaml
-import argparse
-
-from src.preprocess.preprocess import cleaner, lasso
-from src.preprocess_data import harmoniser
-from . import MS_train, dirichlet_old, predict, train_old
-import torch
-from src.preprocess import preprocess
